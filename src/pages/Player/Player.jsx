267d825be92e555b5ca43e1286f1a0e15b70d322
Admin <tuandesigner4287@gmail.com>
2025-04-15 19:13:07 +0700

    Edit player component

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
-  return <div className="player"></div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  });
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YjMyYTMwZjYyMTAxNDAyMzNkMWYzYzkzZjU1M2QwMCIsIm5iZiI6MTc0NDcxMzM0NS4xNTI5OTk5LCJzdWIiOiI2N2ZlMzY4MWFhY2Y3Y2ZiMjY5OWIzMDUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Uvz2noug7pfItoP5E1lMd0ziff9-1jm0aJxkyUsAmuc",
+    },
+  };
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameborder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Player;
